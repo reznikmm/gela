66cb9443ed10d2d673b2d56036c3a9a20d8a5907
Maxim Reznik <reznikmm@gmail.com>
2013-06-22 06:49:37 +0000

    Restore xml_y_driver
    git-svn-id: svn://forge.ada-ru.org/gela@132 139d24a0-51ce-11e1-8fe0-33b25e00b6ad

--- trunk/tools/xml2ayacc/xml_to_y-driver.adb
+++ trunk/tools/xml2ayacc/xml_to_y-driver.adb
@@ -4,10 +4,11 @@ with Ada.Text_IO;
 procedure Xml_To_Y.Driver is
    package C renames Ada.Command_Line;
 begin
-   if C.Argument_Count /= 4 then
+   if C.Argument_Count /= 5 then
       Ada.Text_IO.Put_Line
-        ("Usage : xml_to_y xml_hier.xml tokens.xml fixed.xml code.xml");
+        ("Usage : xml_to_y xml_hier.xml tokens.xml fixed.xml code.xml parser.y");
    else
-      Run (C.Argument (1), C.Argument (2), C.Argument (3), C.Argument (4));
+      Run (C.Argument (1), C.Argument (2), C.Argument (3),
+           C.Argument (4), C.Argument (5));
    end if;
 end Xml_To_Y.Driver;
