4bdcd415a63447d611b7521aff763d0d655b1b84
Maxim Reznik <reznikmm@gmail.com>
2014-02-14 18:47:32 +0000

    Split on_reduce huge case to several smaller procedures
    git-svn-id: svn://forge.ada-ru.org/gela@234 139d24a0-51ce-11e1-8fe0-33b25e00b6ad

--- branches/invoke/tools/ag/yacc_driver.adb
+++ branches/invoke/tools/ag/yacc_driver.adb
@@ -29,9 +29,19 @@ procedure YACC_Driver is
    use type Gela.Grammars.LR.State_Count;
    use type Gela.Grammars.Non_Terminal_Count;
 
+   procedure Put_Proc_Decl
+     (Output : in out Writer;
+      Suffix : Wide_Wide_String);
+
+   procedure Put_Piece
+     (Piece : in out Writer;
+      From  : Gela.Grammars.Production_Index;
+      To    : Gela.Grammars.Production_Index);
+
    procedure Put_Rule
-     (Prod : Gela.Grammars.Production;
-      Rule : Gela.Grammars.Rule);
+     (Output : in out Writer;
+      Prod   : Gela.Grammars.Production;
+      Rule   : Gela.Grammars.Rule);
 
    function Image (X : Integer) return Wide_Wide_String;
 
@@ -268,8 +278,9 @@ procedure YACC_Driver is
    --------------
 
    procedure Put_Rule
-     (Prod : Gela.Grammars.Production;
-      Rule : Gela.Grammars.Rule)
+     (Output : in out Writer;
+      Prod   : Gela.Grammars.Production;
+      Rule   : Gela.Grammars.Rule)
    is
       use Gela.Grammars.Rule_Templates;
       use type League.Strings.Universal_String;
@@ -321,6 +332,111 @@ procedure YACC_Driver is
       Output.P (Template.Substitute (Args));
    end Put_Rule;
 
+   procedure Put_Proc_Decl
+     (Output : in out Writer;
+      Suffix : Wide_Wide_String) is
+   begin
+      Output.N ("procedure Gela.LARL_Parsers.On_Reduce");
+      Output.P (Suffix);
+      Output.P ("  (Self  : access Parser_Context;");
+      Output.P ("   Prod  : Gela.Grammars.Production_Index;");
+      Output.N ("   Nodes : in out " &
+                  "Gela.LARL_Parsers_Nodes.Node_Array)");
+   end Put_Proc_Decl;
+
+   procedure Put_Piece
+     (Piece : in out Writer;
+      From  : Gela.Grammars.Production_Index;
+      To    : Gela.Grammars.Production_Index)
+   is
+      Suffix : Wide_Wide_String :=
+        Gela.Grammars.Production_Index'Wide_Wide_Image (From);
+   begin
+      Suffix (1) := '_';
+
+      Piece.P ("with Gela.Grammars;");
+      Piece.P ("with Gela.LARL_Parsers_Nodes;");
+      Piece.N ("private ");
+      Put_Proc_Decl (Piece, Suffix);
+      Piece.P (";");
+      Piece.N ("pragma Preelaborate (Gela.LARL_Parsers.On_Reduce");
+      Piece.N (Suffix);
+      Piece.P (");");
+      Piece.P;
+
+      Piece.P ("pragma Warnings (""U"");");
+      Piece.P ("with Gela.Elements.Aspect_Specifications;");
+      Piece.P ("with Gela.Elements.Associations;");
+      Piece.P ("with Gela.Elements.Basic_Declarative_Items;");
+      Piece.P ("with Gela.Elements.Case_Expression_Paths;");
+      Piece.P ("with Gela.Elements.Case_Paths;");
+      Piece.P ("with Gela.Elements.Clause_Or_Pragmas;");
+      Piece.P ("with Gela.Elements.Compilation_Units;");
+      Piece.P ("with Gela.Elements.Component_Items;");
+      Piece.P ("with Gela.Elements.Context_Items;");
+      Piece.P ("with Gela.Elements.Declarative_Items;");
+      Piece.P ("with Gela.Elements.Defining_Identifiers;");
+      Piece.P ("with Gela.Elements.Discrete_Choices;");
+      Piece.P ("with Gela.Elements.Discrete_Subtype_Definitions;");
+      Piece.P ("with Gela.Elements.Discriminant_Specifications;");
+      Piece.P ("with Gela.Elements.Enumeration_Literal_Specifications;");
+      Piece.P ("with Gela.Elements.Exception_Choices;");
+      Piece.P ("with Gela.Elements.Exception_Handlers;");
+      Piece.P ("with Gela.Elements.Generic_Associations;");
+      Piece.P ("with Gela.Elements.Generic_Formals;");
+      Piece.P ("with Gela.Elements.If_Else_Expression_Paths;");
+      Piece.P ("with Gela.Elements.If_Elsif_Else_Paths;");
+      Piece.P ("with Gela.Elements.Membership_Choices;");
+      Piece.P ("with Gela.Elements.Names;");
+      Piece.P ("with Gela.Elements.Parameter_Specifications;");
+      Piece.P ("with Gela.Elements.Pragma_Argument_Associations;");
+      Piece.P ("with Gela.Elements.Program_Unit_Names;");
+      Piece.P ("with Gela.Elements.Protected_Element_Declarations;");
+      Piece.P ("with Gela.Elements.Protected_Operation_Declarations;");
+      Piece.P ("with Gela.Elements.Protected_Operation_Items;");
+      Piece.P ("with Gela.Elements.Select_Or_Else_Paths;");
+      Piece.P ("with Gela.Elements.Select_Then_Abort_Paths;");
+      Piece.P ("with Gela.Elements.Statements;");
+      Piece.P ("with Gela.Elements.Subtype_Marks;");
+      Piece.P ("with Gela.Elements.Task_Items;");
+      Piece.P ("with Gela.Elements.Variants;");
+
+      Piece.P ("with Gela.LARL_Parsers_Nodes;");
+      Piece.P ("use Gela.LARL_Parsers_Nodes;");
+      Piece.P ("pragma Style_Checks (""N"");");
+
+      Put_Proc_Decl (Piece, Suffix);
+      Piece.P (" is");
+      Piece.P ("begin");
+      Piece.P ("   case Prod is");
+
+      for Prod of Plain.Production (From .. To) loop
+         Piece.N ("      when");
+         Piece.N (Gela.Grammars.Production_Index'Wide_Wide_Image (Prod.Index));
+         Piece.P (" =>");
+
+         for Rule of Plain.Rule (Prod.First_Rule .. Prod.Last_Rule) loop
+            Put_Rule (Piece, Prod, Rule);
+         end loop;
+
+         if Prod.First_Rule > Prod.Last_Rule then
+            Piece.P ("         null;");
+         end if;
+      end loop;
+
+      Piece.P ("      when others =>");
+      Piece.P ("         raise Constraint_Error;");
+      Piece.P ("   end case;");
+      Piece.N ("end Gela.LARL_Parsers.On_Reduce");
+      Piece.N (Suffix);
+      Piece.P (";");
+   end Put_Piece;
+
+   use type Gela.Grammars.Production_Count;
+
+   Piece_Length : constant Gela.Grammars.Production_Count := 500;
+
+   Piece : Writer;
 begin
    Output.P ("with Gela.Grammars;");
    Output.P ("with Gela.Grammars.LR_Parsers;");
@@ -352,81 +468,60 @@ begin
    Output.P;
    Output.P ("with Gela.Grammars;");
    Output.P ("with Gela.LARL_Parsers_Nodes;");
-   Output.P ("private procedure Gela.LARL_Parsers.On_Reduce");
-   Output.P ("  (Self  : access Parser_Context;");
-   Output.P ("   Prod  : Gela.Grammars.Production_Index;");
-   Output.P ("   Nodes : in out " &
-               "Gela.LARL_Parsers_Nodes.Node_Array);");
+   Output.N ("private ");
+   Put_Proc_Decl (Output, "");
+   Output.P (";");
    Output.P ("pragma Preelaborate (Gela.LARL_Parsers.On_Reduce);");
 
-   Output.P ("with Gela.Elements.Aspect_Specifications;");
-   Output.P ("with Gela.Elements.Associations;");
-   Output.P ("with Gela.Elements.Basic_Declarative_Items;");
-   Output.P ("with Gela.Elements.Case_Expression_Paths;");
-   Output.P ("with Gela.Elements.Case_Paths;");
-   Output.P ("with Gela.Elements.Clause_Or_Pragmas;");
-   Output.P ("with Gela.Elements.Compilation_Units;");
-   Output.P ("with Gela.Elements.Component_Items;");
-   Output.P ("with Gela.Elements.Context_Items;");
-   Output.P ("with Gela.Elements.Declarative_Items;");
-   Output.P ("with Gela.Elements.Defining_Identifiers;");
-   Output.P ("with Gela.Elements.Discrete_Choices;");
-   Output.P ("with Gela.Elements.Discrete_Subtype_Definitions;");
-   Output.P ("with Gela.Elements.Discriminant_Specifications;");
-   Output.P ("with Gela.Elements.Enumeration_Literal_Specifications;");
-   Output.P ("with Gela.Elements.Exception_Choices;");
-   Output.P ("with Gela.Elements.Exception_Handlers;");
-   Output.P ("with Gela.Elements.Generic_Associations;");
-   Output.P ("with Gela.Elements.Generic_Formals;");
-   Output.P ("with Gela.Elements.If_Else_Expression_Paths;");
-   Output.P ("with Gela.Elements.If_Elsif_Else_Paths;");
-   Output.P ("with Gela.Elements.Membership_Choices;");
-   Output.P ("with Gela.Elements.Names;");
-   Output.P ("with Gela.Elements.Parameter_Specifications;");
-   Output.P ("with Gela.Elements.Pragma_Argument_Associations;");
-   Output.P ("with Gela.Elements.Program_Unit_Names;");
-   Output.P ("with Gela.Elements.Protected_Element_Declarations;");
-   Output.P ("with Gela.Elements.Protected_Operation_Declarations;");
-   Output.P ("with Gela.Elements.Protected_Operation_Items;");
-   Output.P ("with Gela.Elements.Select_Or_Else_Paths;");
-   Output.P ("with Gela.Elements.Select_Then_Abort_Paths;");
-   Output.P ("with Gela.Elements.Statements;");
-   Output.P ("with Gela.Elements.Subtype_Marks;");
-   Output.P ("with Gela.Elements.Task_Items;");
-   Output.P ("with Gela.Elements.Variants;");
-
-   Output.P ("with Gela.LARL_Parsers_Nodes;");
-   Output.P ("use Gela.LARL_Parsers_Nodes;");
-   Output.P ("pragma Style_Checks (""N"");");
    Output.P;
-   Output.P ("procedure Gela.LARL_Parsers.On_Reduce");
-   Output.P ("  (Self  : access Parser_Context;");
-   Output.P ("   Prod  : Gela.Grammars.Production_Index;");
-   Output.P ("   Nodes : in out " &
-               "Gela.LARL_Parsers_Nodes.Node_Array) is");
+
+   for Piece_Index in 0 .. (Plain.Last_Production - 1) / Piece_Length loop
+      declare
+         From  : constant Gela.Grammars.Production_Index :=
+           Piece_Index * Piece_Length + 1;
+      begin
+         Output.N ("with Gela.LARL_Parsers.On_Reduce_");
+         Output.N (Natural (From));
+         Output.P (";");
+      end;
+   end loop;
+
+   Put_Proc_Decl (Output, "");
+   Output.P (" is");
    Output.P ("begin");
    Output.P ("   case Prod is");
 
-   for Prod of Plain.Production loop
-      Output.N ("      when");
-      Output.N (Gela.Grammars.Production_Index'Wide_Wide_Image (Prod.Index));
-      Output.P (" =>");
+   for Piece_Index in 0 .. (Plain.Last_Production - 1) / Piece_Length loop
+      declare
+         From  : constant Gela.Grammars.Production_Index :=
+           Piece_Index * Piece_Length + 1;
+         To    : constant Gela.Grammars.Production_Index :=
+           Gela.Grammars.Production_Index'Min
+             (Plain.Last_Production, (Piece_Index + 1) * Piece_Length);
+      begin
+         Output.N ("      when ");
+         Output.N (Natural (From));
+         Output.N (" .. ");
+         Output.N (Natural (To));
+         Output.P (" =>");
+         Output.N ("         On_Reduce_");
+         Output.N (Natural (From));
+         Output.P (" (Self, Prod, Nodes);");
 
-      for Rule of Plain.Rule (Prod.First_Rule .. Prod.Last_Rule) loop
-         Put_Rule (Prod, Rule);
-      end loop;
-
-      if Prod.First_Rule > Prod.Last_Rule then
-         Output.P ("         null;");
-      end if;
+         Put_Piece
+           (Piece => Piece,
+            From  => From,
+            To    => To);
+      end;
    end loop;
 
    Output.P ("      when others =>");
-   Output.P ("         null;");
+   Output.P ("         raise Constraint_Error;");
    Output.P ("   end case;");
    Output.P ("end Gela.LARL_Parsers.On_Reduce;");
 
    Ada.Text_IO.Put_Line (Output.Text.To_UTF_8_String);
+   Ada.Text_IO.Put_Line (Piece.Text.To_UTF_8_String);
 
    Resolver.Resolve (AG, Table.all);
    Gela.Grammars_Debug.Print_Conflicts (AG, Table.all);
